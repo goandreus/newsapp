4e727122d8eeb0da688c95b3a64087b23970464b
goandreus <andreshtml5@gmail.com>
2020-04-04 16:12:51 -0500

    theme and struc

---
+++ lib/src/pages/tabs_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TabsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        physics: BouncingScrollPhysics(),
+        children: <Widget>[
+        
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.green,
+        ),
+      ],)
+    );
+  }
+}
