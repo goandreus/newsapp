c05dd22c2af889ebeacf27be3a87754cbe37cd68
goandreus <andreshtml5@gmail.com>
2020-04-04 16:30:15 -0500

    implements tabs

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -4,17 +4,37 @@ class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        physics: BouncingScrollPhysics(),
-        children: <Widget>[
-        
-        Container(
-          color: Colors.red,
-        ),
-        Container(
-          color: Colors.green,
-        ),
-      ],)
+      body: _Page(),
+      bottomNavigationBar: _Navigation(),
     );
+  }
+}
+
+class _Navigation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.person_outline),title: Text("Para ti")),
+        BottomNavigationBarItem(icon: Icon(Icons.public),title: Text("Encabezados")),
+      ]
+      );
+  }
+}
+
+class _Page extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+      Container(
+        color: Colors.red,
+      ),
+      Container(
+        color: Colors.green,
+      ),
+    ],);
   }
 }
