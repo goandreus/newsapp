fb163f841b64059322790c6371352d525292c61d
goandreus <andreshtml5@gmail.com>
2020-04-05 15:27:39 -0500

    charge news

--- lib/src/pages/tabs1_page.dart
+++ lib/src/pages/tabs1_page.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:newsapp/src/services/news_service.dart';
+import 'package:newsapp/src/widgets/list_news.dart';
 import 'package:provider/provider.dart';
 
 class Tab1Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final newsService = Provider.of<NewsService>(context);
+
+    final headlines = Provider.of<NewsService>(context).headlines;
+
     return Scaffold(
-      body: Center(child: Text("Hellow Word  "),),
+      body: ListNews(headlines)
     );
   }
 }
