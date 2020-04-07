165045284d8e9f496faa5523c45876dbaa19cad7
goandreus <andreshtml5@gmail.com>
2020-04-06 19:04:06 -0500

    add categories rest

--- lib/src/pages/tabs2_page.dart
+++ lib/src/pages/tabs2_page.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp/src/services/news_service.dart';
+import 'package:provider/provider.dart';
 
 class Tab2Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("hi"),
+    return SafeArea(
+          child: Scaffold(
+        body: Column( 
+          children: <Widget>[
+            Expanded(child: _ListCategory())
+          ],
+        ),
       ),
     );
+  }
+}
+
+class _ListCategory extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    final categories = Provider.of<NewsService>(context).categories;
+
+    return ListView.builder(
+      physics: BouncingScrollPhysics(),
+      scrollDirection: Axis.horizontal ,
+      itemCount: categories.length,
+      itemBuilder: (BuildContext context, int index){
+        return Padding(
+          padding: EdgeInsets.all(8),
+            child: Column(
+              children: <Widget>[
+                Icon(categories[index].icon),
+                SizedBox(height: 5,),
+                Text(categories[index].name)
+              ],
+            ),
+          );
+      },
+      );
   }
 }
--- lib/src/services/news_service.dart
+++ lib/src/services/news_service.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:newsapp/src/models/category_model.dart';
 import 'package:newsapp/src/models/news_models.dart';
 import 'package:http/http.dart' as http;
 
@@ -8,6 +10,16 @@ final _APIKEY = '42bd9676e64f4f2f81b64cc88d038b84';
 
 class NewsService with ChangeNotifier {
   List<Article> headlines = [];
+
+  List<Category> categories = [
+    Category(FontAwesomeIcons.building, 'business'),
+    Category(FontAwesomeIcons.tv, 'entertaiment'),
+    Category(FontAwesomeIcons.addressCard, 'general'),
+    Category(FontAwesomeIcons.headSideVirus, 'health'),
+    Category(FontAwesomeIcons.vials, 'science'),
+    Category(FontAwesomeIcons.volleyballBall, 'sports'),
+    Category(FontAwesomeIcons.memory, 'technology'),
+  ];
   
   NewsService(){
     this.getTopHeadlines();
